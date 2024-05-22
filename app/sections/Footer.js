d2a627aff04121129a411a21c661ec9a04b9e2a1
unwontedsponn <benpaulspooner@gmail.com>
2024-05-22 15:23:06 +0100

    Created Footer compoennt

---
+++ app/sections/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <main className="bg-gray-100">
+      <div className="grid grid-cols-3 grid-flow-col gap-4">
+        <p>I'm a footer</p>
+        <p>I'm a footer</p>
+        <p>I'm a footer</p>
+      </div>
+    </main>
+  )
+}
